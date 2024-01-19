db02f785c968146c8271ed4c5e5ce5aa394c5ac5
bw1611 <97327583+leebuwon@users.noreply.github.com>
2024-01-19 18:15:39 +0900

    Feat: apigateway 설정 (#9)

---
+++ backend/apigatewayservice/src/test/java/com/tadak/apigatewayservice/ApigatewayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadak.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
