102ea333bf65e82ef5c371f9ca8363bf3f896d27
김규회 <48755156+KimKyuHoi@users.noreply.github.com>
2024-01-20 16:21:55 +0900

    Feat/issue-1 (#13)
    * Feat : Vite + TypeScript 설치
    
    * Feat : pnpm 설치
    
    * Feat : React-Query 설치 및 기본 세팅
    
    * Feat :  ESLint(airbnb) 적용 및 Prettier 세부 설정

---
+++ frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<QueryClientProvider client={queryClient}>
+		<ReactQueryDevtools initialIsOpen={true} />
+		<App />
+	</QueryClientProvider>,
+);
