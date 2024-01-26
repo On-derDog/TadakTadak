36eee0cdc06fe1f4d9b910089595ed7ecd8a98d7
DingX2 <pourding@naver.com>
2024-01-26 17:49:32 +0900

    Feat: API 코드 구현 / URL 연결필요

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -5,23 +5,29 @@ import { Button } from "../components/Button";
 import { useStore, create } from "zustand";
 import { UserInfoStore } from "../stores/UserInfoStore";
 import { devtools } from 'zustand/middleware'
+import axios from 'axios';
+
+
 
 const SignupPage = () => {
     const navigate = useNavigate();
     const userInfo = useStore(UserInfoStore);
     let passwordCheck:string;
+    let message:string;
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { value, name } = e.target;
 
         if (name === 'email') {
-          userInfo.updateEmail(value); 
+          userInfo.updateEmail(value);
         } else if (name === 'password') {
           userInfo.updatePassword(value);
         }
         else if (name === 'password-check') {
             if(userInfo.password === value){
-                console.log("비밀번호가 동일합니다.")
+                console.log("일치하는 비밀번호 입니다.")
+            }else{
+                console.log("비밀번호가 일치하지 않습니다.")
             }
         }
         else if (name === 'username') {
@@ -29,20 +35,37 @@ const SignupPage = () => {
         }
     }
 
+    // 통신코드
+    const checkEmailDuplicate = async () => {
+        try {
+          const response = await axios.post('/api/v1/signup', {
+            username: userInfo.username,
+            email: userInfo.email,
+            password: userInfo.password
+          });
+      
+          const data = response.data;
+      
+        } catch (error) {
+          console.error("API 통신 에러:", error);
+        }
+      };
+
     const onButtonClick = (action:string) => {
         switch(action){
-          case "onSignupEmail":
+          case "onSetEmailCheck":
             console.log(`API 통신 ${userInfo.email}`)
             break;
-          case "onSetNickname":
-            console.log(`API 통신 ${userInfo.username}`)
-            break;
-          case "onSignup":
-            console.log(`회원가입 ${userInfo.email}, ${userInfo.password}, ${userInfo.username}`);
-            setTimeout(()=> navigate("/"),2000);
-            break;
-          default:
-            console.error("error")
+            case "onSetNicknameCheck":
+                console.log(`API 통신 ${userInfo.username}`)
+                break;
+            case "onSignup":
+                console.log(`회원가입 ${userInfo.email}, ${userInfo.password}, ${userInfo.username}`);
+                checkEmailDuplicate();
+                setTimeout(()=> navigate("/"),2000);
+                break;
+            default:
+                console.error("error")
         }
     }
 
@@ -55,7 +78,8 @@ const SignupPage = () => {
   
             {/* email input */}
             <InputForm onChange={onChange}type = 'text' title="이메일"  name="email" value={userInfo?.email} placeholder="이메일을 입력해주세요"/>
-            <Button onClick={ () => onButtonClick("onSignupEmail")} label={"중복확인"}></Button>
+            <Button onClick={ () => onButtonClick("onSetEmailCheck")} label={"중복확인"}></Button>
+            {/* <span> {message} </span> */}
             <br/>            
   
             {/* pw input */}
@@ -71,7 +95,7 @@ const SignupPage = () => {
             {/* username input */}
             <InputForm onChange={onChange}
             type = 'text' title="닉네임"  name="username" value={userInfo?.username} placeholder="닉네임을 입력해주세요"/>
-            <Button onClick={ () => onButtonClick("onSetNickname")} label={"중복 확인"}></Button>
+            <Button onClick={ () => onButtonClick("onSetNicknameCheck")} label={"중복 확인"}></Button>
             <br/>
             <br/>
             <Button onClick={ () => onButtonClick("onSignup")} label={"회원가입"}></Button>
