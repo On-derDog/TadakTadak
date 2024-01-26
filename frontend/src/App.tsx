c1e849aa21731f5adc36d18326893548e13faf96
eastfilmm <ehdvlf001@knu.ac.kr>
2024-01-26 14:06:03 +0900

    Feat: 소켓연결 이슈 수정 중

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
 import ChattingRoomPage from './pages/ChattingRoomPage';
 import TestPage from './test/TestPage';
-import Video from './components/video/Video';
+import Video from './components/video/VideoCall';
 
 const router = createBrowserRouter([
 	{
