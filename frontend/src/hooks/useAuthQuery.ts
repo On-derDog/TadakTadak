ceef46e0711b9de69848248f921d08c90ed6cca0
DingX2 <pourding@naver.com>
2024-01-29 04:04:37 +0900

    Feat: refreshToken interceptor 추가

--- frontend/src/hooks/useAuthQuery.ts
+++ frontend/src/hooks/useAuthQuery.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios,{ AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { UserInfo, UserInfoStore } from '../stores/UserInfoStore';
 import { useStore } from 'zustand';
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
@@ -42,7 +42,7 @@ export const AuthApis = {
 
   signup: async (userInfo:UserInfo, passwordConfirm: string) => {
     try {
-      const response = await AuthApis.instance.post('/login', {
+      const response = await AuthApis.instance.post('/signup', {
         username: userInfo.username,
         email: userInfo.email,
         password: userInfo.password,
@@ -50,7 +50,11 @@ export const AuthApis = {
       });
       const data = response.data;
       const jwtToken = data.token;
+      const refreshToken = data.refreshToken;
+  
       localStorage.setItem('jwtToken', jwtToken);
+      localStorage.setItem('refreshToken', refreshToken);
+
       return data;
     } catch (error) {
       console.error("API 통신 에러:", error);
@@ -65,21 +69,83 @@ export const AuthApis = {
     return { data, isLoading, error };
   },
 
+
+  signin: async (userInfo:UserInfo, passwordConfirm: string) => {
+    try {
+      const response = await AuthApis.instance.post('/login', {
+        email: userInfo.email,
+        password: userInfo.password,
+      });
+      const data = response.data;
+      const jwtToken = data.token;
+      const refreshToken = data.refreshToken;
+  
+      localStorage.setItem('jwtToken', jwtToken);
+      localStorage.setItem('refreshToken', refreshToken);
+
+      return data;
+    } catch (error) {
+      console.error("API 통신 에러:", error);
+    }
+  },
+
+  //refreshToken
+  setupInterceptors: () => {
+    AuthApis.instance.interceptors.response.use(
+      (response: AxiosResponse) => response,
+      async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response.status === 401 && !originalRequest._retry) {
+          originalRequest._retry = true;
+
+          try {
+            const refreshToken = localStorage.getItem('refreshToken');
+            // 재발급받는 경로
+            const response = await AuthApis.instance.post<{ accessToken: string }>('/login', { refreshToken });
+
+            const newAccessToken = response.data.accessToken;
+
+            originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+
+            return AuthApis.instance(originalRequest);
+          } catch (refreshError) {
+            console.error("토큰 갱신 실패:", refreshError);
+            localStorage.removeItem('jwtToken');
+            localStorage.removeItem('refreshToken');
+            window.location.href = '/signin';
+          }
+        }
+
+        return Promise.reject(error);
+      }
+    );
+  },
+
+
+  // 리팩토링용 코드
   fetchData: async (endpoint: string, type: string, data?: any) => {
     let response;
   
     switch (type) {
       case 'get':
-        response = await axios.get(`http://localhost:8001/user-service/${endpoint}`);
+        response = await AuthApis.instance.get(`http://localhost:8001/user-service/${endpoint}`, {
+          headers: { Authorization: `Bearer ${localStorage.getItem('jwtToken')}` },
+        });
         break;
+
       case 'post':
-        response = await axios.post(`http://localhost:8001/user-service/${endpoint}`, data);
+        response = await AuthApis.instance.post(`http://localhost:8001/user-service/${endpoint}`, data, {
+          headers: { Authorization: `Bearer ${localStorage.getItem('jwtToken')}` },
+        });
         break;
+
       default:
         console.error("Invalid type:", type);
     }
+  
     return response;
-  }
+  },
   
 };
 
