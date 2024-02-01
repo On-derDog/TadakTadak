81cbb3f56acc3b12bcd728719fead372d55b3bb5
bw1611 <bw1611@naver.com>
2024-02-01 11:34:15 +0900

    Feat: config ExposedHeader 추가

--- backend/user-service/src/main/java/com/tadak/userservice/global/security/config/CorsConfig.java
+++ backend/user-service/src/main/java/com/tadak/userservice/global/security/config/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
         config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+        config.addExposedHeader("Accesstoken, Refreshtoken");
 
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
