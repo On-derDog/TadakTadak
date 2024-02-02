ab146475c3c05a0224f3334f0347f14c4618517f
KimKyuHoi <k546kh@gmail.com>
2024-02-02 16:47:03 +0900

    Design: CSS layout 설정

--- frontend/src/pages/ChattingRoomPage.tsx
+++ frontend/src/pages/ChattingRoomPage.tsx
@@ -1,5 +1,32 @@
+// import Layout from '../components/layout/Layout';
+import { Container, Wrapper, SideWrapper, MainWrapper } from '../styles/Layout';
+import styled from '@emotion/styled';
+
 const ChattingRoomPage = () => {
-	return <>It's Chatting RoomPage!</>;
+	return (
+		<Container>
+			<Wrapper>
+				<SideWrapper>채팅방이름</SideWrapper>
+				<MainWrapper>
+					<VideoWrapper>비디오</VideoWrapper>
+					<ChatWrapper>채팅</ChatWrapper>
+				</MainWrapper>
+				<SideWrapper>인원 목록</SideWrapper>
+			</Wrapper>
+		</Container>
+	);
 };
 
 export default ChattingRoomPage;
+
+export const VideoWrapper = styled.div`
+	width: 50%;
+	height: 100%;
+	background-color: lightblue;
+`;
+
+export const ChatWrapper = styled.div`
+	width: 50%;
+	height: 100%;
+	background-color: lightgreen;
+`;
