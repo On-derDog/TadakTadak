bb8ee95ffbb0d1463f426b4e912fb502a3ad2301
bw1611 <bw1611@naver.com>
2024-02-07 15:59:31 +0900

    Feat: Chatroom service 초기 구현

---
+++ backend/chatroom-service/src/test/java/com/tadak/chatroomservice/ChatroomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadak.chatroomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
