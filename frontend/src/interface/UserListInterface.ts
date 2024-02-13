cd03ec02d6c97f2a30176d7874f8e58ff7c5589f
KimKyuHoi <k546kh@gmail.com>
2024-02-13 20:05:03 +0900

    Feat: UserData를 받아서 userlist와 chattingroom에 뿌려주기

--- frontend/src/interface/UserListInterface.ts
+++ frontend/src/interface/UserListInterface.ts
@@ -1,3 +1,15 @@
 export interface UserList {
 	userName: string;
 }
+
+export interface UserData {
+	memberId: string;
+	email: string;
+	username: string;
+}
+
+interface UserDataProps {
+	username: string | undefined;
+	isLoading: boolean;
+	isError: boolean;
+}
