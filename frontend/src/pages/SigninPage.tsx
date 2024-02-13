7c680ca89304b01fdf1a2de03cab988aab89fa47
DingX2 <pourding@naver.com>
2024-02-13 17:04:06 +0900

    Feat: InputForm CSS 적용

--- frontend/src/pages/SigninPage.tsx
+++ frontend/src/pages/SigninPage.tsx
@@ -5,28 +5,11 @@ import { Button } from "../components/Button";
 import { NaverLoginButton } from "../components/auth/NaverLoginButton";
 import { useStore } from "zustand";
 import { UserInfoStore } from "../stores/UserInfoStore";
-
 import { AuthApis }  from "../hooks/useAuthQuery";
+import styled from "@emotion/styled"
+import LogoSVG from "../assets/Logo.svg"
+import TadakTadakSVG from "../assets/TadakTadak.svg"
 import axios from "axios";
-
-
-const fetchDataExample = async () => {
-    instance: axios.create({
-        baseURL: "http://localhost:8001/user-service/",
-        withCredentials: true,
-      })
-
-      try {
-        AuthApis.setupInterceptors();
-        const response = await AuthApis.instance.get('/hello',  {
-            headers: { Accesstoken: `Bearer ${localStorage.getItem('Accesstoken')}` },
-          });
-        const data = response.data;
-        console.log("로그인 성공")
-        return data;
-      } catch (error) {
-      }
-  };
 
 const SigninPage = () => {
     const navigate = useNavigate();
@@ -51,43 +34,65 @@ const SigninPage = () => {
         case "onSignup":
             navigate("/signup")
             break;
-        case "onSignupKakao":
-            fetchDataExample();
-            // navigate("/signupnaver")
-            break;
         default:
             console.error("error")
       }
     }
 
     return(
-      <main className="SigninPage-wrapper">
-        <section className="SiginPage-container">
-          <h1> TadakTadak Logo </h1>
-          <>It's SigninPage!</>
+      <SignInWrapper>
+        <SignInContainer>
+          <LogoContainer>
+            <figure className="Logo"> 
+              <img src={LogoSVG} alt="logo"/>
+              <figcaption>
+                <img src={TadakTadakSVG} alt="TadakTadak"/>
+              </figcaption>
+            </figure>
+          </LogoContainer>
           <br/>    
 
           {/* email input */}
-          <InputForm onChange={onChange}type = 'text' title="이메일"  name="email" value={userInfo?.email} placeholder="이메일을 입력해주세요"/>
+          <InputForm onChange={onChange}type = 'text' title="이메일" imgSVG="Email"  name="email" value={userInfo?.email} placeholder="이메일을 입력해주세요"/>
 
           <br/>            
-
           {/* pw input */}
           <InputForm onChange={onChange}
-          type = 'password' title="비밀번호"  name="password" value={userInfo?.password} placeholder="비밀번호을 입력해주세요"/>
+          type = 'password' imgSVG="Password" title="비밀번호"  name="password" value={userInfo?.password} placeholder="비밀번호을 입력해주세요"/>
 
           <br/>
           <Button onClick={ () => onButtonClick("onSignin")} label={"로그인"}></Button>
           <br/>
           <Button onClick={ () => onButtonClick("onSignup")} label={"회원가입"}></Button>
           <br/>
-          <Button onClick={ () => onButtonClick("onSignupKakao")} label={"테스트용코드"}></Button>
-          <br/>
           <NaverLoginButton/>
-        </section>  
-      </main>
+        </SignInContainer>  
+      </SignInWrapper>
   )
   
 };
 
 export default SigninPage;
+
+const SignInWrapper = styled.main`
+  display: flex;
+  padding: 2rem;
+  align-items: center;
+  justify-content: center;
+`
+
+const SignInContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+`
+
+const LogoContainer = styled.figure`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 191px;
+  }
+`
