e7d03bd9ba31f6d53e67260c4b2882c3ada06959
DingX2 <pourding@naver.com>
2024-02-15 15:57:33 +0900

    Fix: layout 복구

--- frontend/src/styles/Layout.ts
+++ frontend/src/styles/Layout.ts
@@ -27,13 +27,13 @@ export const SideWrapper = styled.div`
 	margin: 0.5rem 0rem 0.5rem 0rem;
 `;
 
-export const FlexCenterWrapper = styled.div`
+export const FlexCenterWrapper =`
 	display: flex;
 	justify-content: center;
 	align-items: center;
 `;
 
-export const ChattingRoomHeader = styled.div`
+export const ChattingRoomHeader =`
 	background-color: var(--color-pumpkin);
 	color: var(--color-white);
 	border-radius: 5px 5px 0px 0px;
